a9b8dda2be946d78e59fd5ebe363d1201781b6df
Thisaru-Sach <sachthisa@gmail.com>
2026-03-28 14:22:21 +0530

    Word replace

--- src/components/Organizers.jsx
+++ src/components/Organizers.jsx
@@ -80,7 +80,7 @@ function Organizers() {
                 <line x1="2" y1="12" x2="22" y2="12"></line>
                 <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
               </svg>
-              statsociety@sci.sjp.ac.lk
+              Web
             </a>
             {/* REPLACE # WITH ACTUAL LINKEDIN URL */}
             <a
